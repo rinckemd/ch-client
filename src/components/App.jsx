12054fd13c78cdb9f87855e6147766a8499b09b8
Rincker, Miles <Miles.Rincker@inin.com>
2017-05-14 11:53:46 -0400

    Added sass support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
+/* @flow */
+
 import React from 'react';
+import app from '../styles/App.scss'
 
 export default class App extends React.Component {
   render() {
     return (
-      <div style={{textAlign: 'center'}}>
+      <div className={app.App}>
         <h1>Hello World</h1>
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './src/index.html',
@@ -11,13 +13,26 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'ch-client-bundle.js'
+    filename: '[name].js'
   },
+  devtool: 'source-map',
   module: {
     loaders: [
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader'
+        })
+      },
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name].css'
+    }),
+    HtmlWebpackPluginConfig
+  ]
 }
